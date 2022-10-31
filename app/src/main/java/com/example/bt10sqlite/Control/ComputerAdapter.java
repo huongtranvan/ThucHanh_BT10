746db6d60321d841a16f2c4533a9d7d145ebd9ee
longnguu <levanlong29062002@gmail.com>
2022-10-31 19:52:15 +0700

    first commit

--- app/src/main/java/com/example/bt10sqlite/Control/ComputerAdapter.java
+++ app/src/main/java/com/example/bt10sqlite/Control/ComputerAdapter.java
@@ -1,19 +1,30 @@
 package com.example.bt10sqlite.Control;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.opengl.Visibility;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.bt10sqlite.ListComputer;
+import com.example.bt10sqlite.MainActivity;
 import com.example.bt10sqlite.Model.Computer;
 import com.example.bt10sqlite.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerAdapter extends RecyclerView.Adapter<ComputerAdapter.ViewHolder> {
@@ -49,27 +60,100 @@ public class ComputerAdapter extends RecyclerView.Adapter<ComputerAdapter.ViewHo
         else return 0;
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         private View itemview;
         public TextView studentname;
         public TextView birthyear;
-        public Button detail_button;
+        public Button btnDel,btnEdit;
 
         public ViewHolder(View itemView) {
             super(itemView);
             itemview = itemView;
             studentname = itemView.findViewById(R.id.studentname);
             birthyear = itemView.findViewById(R.id.birthyear);
-            detail_button = itemView.findViewById(R.id.detail_button);
+            btnDel = itemView.findViewById(R.id.btnDel);
+            btnEdit = itemview.findViewById(R.id.btnEdit);
+
 
             //Xử lý khi nút Chi tiết được bấm
-            detail_button.setOnClickListener(new View.OnClickListener() {
+            btnDel.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(view.getContext(),
-                                    studentname.getText() +" | "
-                                            + " Demo function", Toast.LENGTH_SHORT)
-                            .show();
+                    AlertDialog.Builder alertDiaLog = new AlertDialog.Builder(view.getContext());
+                    alertDiaLog.setTitle("Thông báo");
+                    alertDiaLog.setMessage("Bạn có muốn xóa "+studentname.getText().toString().trim()+" ?"    );
+                    alertDiaLog.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ListComputer.db.QueryData("delete from Computer where idComputer ='"+studentname.getText().toString().trim()+"'");
+                            ListComputer.computers = new ArrayList<Computer>();
+                            Cursor cursor = ListComputer.db.GetData("Select * from Computer where idCategory = '"+ListComputer.idTruyen.toString().trim() +"'");
+                            while (cursor.moveToNext()){
+                                @SuppressLint("Range") String idC =cursor.getString(cursor.getColumnIndex("idComputer"));
+                                @SuppressLint("Range") String nameC = cursor.getString(cursor.getColumnIndex("nameComputer"));
+                                @SuppressLint("Range") String idCategory = cursor.getString(cursor.getColumnIndex("idCategory"));
+                                System.out.println(idC+" "+nameC+" "+idCategory);
+                                ListComputer.computers.add(new Computer(idC,nameC,idCategory));
+//            computers.add(new Computer(cursor.getString(cursor.getColumnIndex("idComputer")),cursor.getString(cursor.getColumnIndex("nameComputer")),cursor.getString(cursor.getColumnIndex("idCategory"))));
+                            }
+                            ListComputer.adapter = new ComputerAdapter(ListComputer.computers, view.getContext());
+                            GridLayoutManager linearLayoutManager = new GridLayoutManager(view.getContext(), 1);
+                            ListComputer.recyclerView.setAdapter(ListComputer.adapter);
+                            ListComputer.recyclerView.setLayoutManager(linearLayoutManager);
+                        }
+                    });
+                    alertDiaLog.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    alertDiaLog.show();
+                }
+            });
+            btnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Dialog dialog = new Dialog(view.getContext());
+                    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                    dialog.setContentView(R.layout.dialogcomputercustom);
+                    dialog.show();
+                    TextView tv1 = (TextView) dialog.findViewById(R.id.isIDC);
+                    TextView tv2 = (TextView) dialog.findViewById(R.id.isNameC);
+                    TextView tv3 = (TextView) dialog.findViewById(R.id.isIDCate);
+                    tv3.setVisibility(View.GONE);
+                    Button btok = (Button) dialog.findViewById(R.id.btn_okC);
+                    Button btcancel = (Button) dialog.findViewById(R.id.btn_cancelC);
+
+                    btok.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ListComputer.db.QueryData("update Computer set idComputer = '"+tv1.getText().toString().trim()+ "',nameComputer='"+ tv2.getText().toString().trim()+ "'  where idComputer ='"+studentname.getText().toString().trim()+"'");
+                            ListComputer.computers = new ArrayList<Computer>();
+                            Cursor cursor = ListComputer.db.GetData("Select * from Computer where idCategory = '"+ListComputer.idTruyen.toString().trim() +"'");
+                            while (cursor.moveToNext()){
+                                @SuppressLint("Range") String idC =cursor.getString(cursor.getColumnIndex("idComputer"));
+                                @SuppressLint("Range") String nameC = cursor.getString(cursor.getColumnIndex("nameComputer"));
+                                @SuppressLint("Range") String idCategory = cursor.getString(cursor.getColumnIndex("idCategory"));
+                                System.out.println(idC+" "+nameC+" "+idCategory);
+                                ListComputer.computers.add(new Computer(idC,nameC,idCategory));
+//            computers.add(new Computer(cursor.getString(cursor.getColumnIndex("idComputer")),cursor.getString(cursor.getColumnIndex("nameComputer")),cursor.getString(cursor.getColumnIndex("idCategory"))));
+                            }
+                            ListComputer.adapter = new ComputerAdapter(ListComputer.computers, view.getContext());
+                            GridLayoutManager linearLayoutManager = new GridLayoutManager(view.getContext(), 1);
+                            ListComputer.recyclerView.setAdapter(ListComputer.adapter);
+                            ListComputer.recyclerView.setLayoutManager(linearLayoutManager);
+                            dialog.dismiss();
+
+                        }
+                    });
+                    btcancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.cancel();
+                        }
+                    });
                 }
             });
         }
--- app/src/main/java/com/example/bt10sqlite/MainActivity.java
+++ app/src/main/java/com/example/bt10sqlite/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.bt10sqlite;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.bt10sqlite.Control.Database;
 import com.example.bt10sqlite.Model.Category;
@@ -20,6 +26,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     Database db;
     List<Category> categories = new ArrayList<Category>();
+    ArrayAdapter<Category> lopArrayAdapter;
+    boolean kt=true;
+    ListView lvCategory;
 
     @SuppressLint("Range")
     @Override
@@ -44,28 +53,90 @@ public class MainActivity extends AppCompatActivity {
             db.insertC("PC04","Computer Asus 4","Category 1");
         }
 
-        ListView lvLop = (ListView) findViewById(R.id.listviewCategory);
+         lvCategory= (ListView) findViewById(R.id.listviewCategory);
 
-        cursor = db.GetData("select * from Category");
+        getDataCategory();
+        lvCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(MainActivity.this, ListComputer.class);
+                    intent.putExtra("idCategory", categories.get(i).getIdCategory());
+                    System.out.println(categories.get(i).getIdCategory());
+                    kt=true;
+                    startActivity(intent);
+            }
+        });
+
+        lvCategory.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int j=i;
+                AlertDialog.Builder alertDiaLog = new AlertDialog.Builder(MainActivity.this);
+                alertDiaLog.setTitle("Thông báo");
+                alertDiaLog.setMessage("Bạn có muốn xóa "+categories.get(i).getIdCategory()+" ?"    );
+                alertDiaLog.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        db.QueryData("delete from Category where idCategory ='"+categories.get(j).getIdCategory()+"'");
+                        getDataCategory();
+                        lopArrayAdapter.notifyDataSetChanged();
+                    }
+                });
+                alertDiaLog.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alertDiaLog.show();
+                return true;
+            }
+        });
+        Button button = (Button) findViewById(R.id.btn_themCategory);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.dialogcategorycustom);
+                dialog.show();
+                TextView tv1 = (TextView) dialog.findViewById(R.id.isIDCa);
+                TextView tv2 = (TextView) dialog.findViewById(R.id.isNameCa);
+                Button btok = (Button) dialog.findViewById(R.id.btn_okCa);
+                Button btcancel = (Button) dialog.findViewById(R.id.btn_cancelCa);
+                btok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        db.insertCa(tv1.getText().toString().trim(),tv2.getText().toString().trim());
+                        dialog.dismiss();
+                        getDataCategory();
+                        lopArrayAdapter.notifyDataSetChanged();
+                    }
+                });
+                btcancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.cancel();
+                    }
+                });
+            }
+        });
+    }
+
+    public void getDataCategory() {
+        categories = new ArrayList<Category>();
+        Cursor cursor = db.GetData("select * from Category");
         System.out.println("ABC " + cursor.getCount());
         if (cursor.getCount()>0){
             System.out.println("abc");
             while (cursor.moveToNext()){
-                String idCa = cursor.getString(cursor.getColumnIndex("idCategory"));
-                String nameCa = cursor.getString(cursor.getColumnIndex("nameCategory"));
+                @SuppressLint("Range") String idCa = cursor.getString(cursor.getColumnIndex("idCategory"));
+                @SuppressLint("Range") String nameCa = cursor.getString(cursor.getColumnIndex("nameCategory"));
                 categories.add(new Category(idCa,nameCa));
             }
         }
-        ArrayAdapter<Category> lopArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,categories);
-        lvLop.setAdapter(lopArrayAdapter);
-        lvLop.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(MainActivity.this,ListComputer.class);
-                intent.putExtra("idCategory",categories.get(i).getIdCategory());
-                System.out.println(categories.get(i).getIdCategory());
-                startActivity(intent);
-            }
-        });
+        lopArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,categories);
+        lvCategory.setAdapter(lopArrayAdapter);
+        lopArrayAdapter.notifyDataSetChanged();
     }
 }
